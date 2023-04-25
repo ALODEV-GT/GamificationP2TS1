db8fbec43e641f51e02712884e1a8fce4d3297ad
Brayan Alonzo <brayan.quialo@gmail.com>
2023-04-24 22:51:04 -0600

    Se creo la base del proyecto, enrutamiento y estructura de carpetas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
-const routes: Routes = [];
+const rutas: Routes = [
+  {
+    //LazyLoad
+    path: 'autenticacion',
+    loadChildren: () => import('./autenticacion/autenticacion.module').then(
+      m => m.AutenticacionModule
+    )
+  },
+  {
+    path: 'estudiante',
+    loadChildren: () => import('./usuarios/estudiante/estudiante.module').then(
+      m => m.EstudianteModule
+    )
+  },
+  {
+    path: 'profesor',
+    loadChildren: () => import('./usuarios/profesor/profesor.module').then(
+      m => m.ProfesorModule
+    )
+  },
+  {
+    path: '', redirectTo: 'autenticacion/login', pathMatch: 'full'
+  },
+  {
+    path: '404', //Pagina de error
+    component: ErrorPageComponent
+  },
+  {
+    path: '**', //Cualquier otra pagina que no existe.
+    redirectTo: '404'
+  }
 
+]
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(rutas)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/usuarios/estudiante/estudiante-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const rutas: Routes = [
+  {
+    path: '',
+    //component: Component0,
+    children: [
+      {
+        path: 'path1',
+        //component: Component1
+      },
+      {
+        path: 'path2',
+        //component: Component2
+      },
+      {
+        path: 'path3',
+        //component: Component3
+      },
+      {
+        path: 'path4',
+        //component: Component4
+      },
+    ]
+  },
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(rutas)
+  ], exports: [
+    RouterModule
+  ]
+})
+export class EstudianteRoutingModule { }
