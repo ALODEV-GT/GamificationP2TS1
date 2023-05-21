48eb6486bcac837a111d97044abb6cf72e67d321
Brayan Alonzo <brayan.quialo@gmail.com>
2023-05-20 20:22:34 -0600

    Se integro crear-partida-comido en profesor

--- UI-Gamification/src/app/usuarios/profesor/profesor-routing.module.ts
+++ UI-Gamification/src/app/usuarios/profesor/profesor-routing.module.ts
@@ -5,6 +5,7 @@ import { MisAulasComponent } from './pages/mis-aulas/mis-aulas.component';
 import { AulaComponent } from './pages/aula/aula.component';
 import { AreaCreacionComponent } from './pages/area-creacion/area-creacion.component';
 import { PerfilComponent } from '../components/perfil/perfil.component';
+import { CreacionComidoComponent } from './pages/creacion-comido/creacion-comido.component';
 
 const rutas: Routes = [
   {
@@ -21,7 +22,12 @@ const rutas: Routes = [
       },
       {
         path: 'area-creacion',
-        component: AreaCreacionComponent
+        component: AreaCreacionComponent,
+
+      },
+      {
+        path: 'area-creacion/comido',
+        component: CreacionComidoComponent
       },
       {
         path: 'perfil',
